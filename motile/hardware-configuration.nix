8d66e1ebcfd48dc17f4cf055544a0421a421f7fe
Christopher Rosset <chris@rosset.org.uk>
2020-02-19 19:13:54 -0500

    backlight fix for motile

--- motile/hardware-configuration.nix
+++ motile/hardware-configuration.nix
@@ -13,6 +13,9 @@
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
 
+  # https://forum.manjaro.org/t/resolution-help-needed-systemd-backlight-backlight-acpi-video0-service-loaded-failed-failed/68131
+  boot.kernelParams = [ "acpi_backlight=native" ];
+
   fileSystems."/" =
     { device = "/dev/disk/by-uuid/b71e2012-58f1-4068-9651-739116ae45d1";
       fsType = "ext4";
