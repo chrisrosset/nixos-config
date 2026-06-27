941ecf83d5668b88d2f467abee023ecf323bc5ce
Christopher Rosset <chris@rosset.tech>
2026-06-27 17:56:03 -0400

    morgoth: enable IP forwarding

--- hardware/morgoth.nix
+++ hardware/morgoth.nix
@@ -18,6 +18,13 @@
     kernelModules = [ "kvm-intel" ];
     extraModulePackages = [ ];
     vesa = true;
+
+    kernel = {
+      sysctl = {
+        "net.ipv4.conf.all.forwarding" = true;
+        "net.ipv6.conf.all.forwarding" = true;
+      };
+    };
   };
 
   fileSystems = {
