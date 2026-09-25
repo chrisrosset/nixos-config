cf5decaa20c661571bec4f5e02ae2a4bbee955b4
Christopher Rosset <chris@rosset.org.uk>
2026-09-25 23:18:25 +0000

    tiamat: sync deployed service configuration

--- hosts/tiamat/default.nix
+++ hosts/tiamat/default.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:
 let
-  # syncthingCfg = import ../../modules/syncthing.nix;
+  syncthingCfg = import ../../modules/syncthing.nix;
   transmissionPath = "/srv/raid/export/transmission";
 in
 {
@@ -46,25 +46,70 @@ in
 
     ntp.enable = true;
 
-    samba = {
-      enable   = false;
-      nsswins  = true;
-      settings = ''
-workgroup = WORKGROUP
-server string = Home server
-security = user
-map to guest = Bad User
-guest account = ctr
+    syncthing = {
+      enable = true;
+      systemService = true;
+      openDefaultPorts = true;
+      user = "ctr";
+      group = "users";
+      dataDir = "/srv/raid/export/syncthing";
+      configDir = "/home/ctr/.config/syncthing";
+      guiAddress = "0.0.0.0:8384";
+      key = "/root/syncthing/key.pem";
+      cert = "/root/syncthing/cert.pem";
+      overrideDevices = true;
+      overrideFolders = true;
+      settings = {
+        devices = syncthingCfg.devices;
+        folders = {
+          "/srv/raid/export/syncthing/tidemill-sync" = {
+            id = "tidemill-sync";
+            label = "tidemill-sync";
+            devices = [ "tiamat" "tidemill" ];
+          };
+        };
+      };
+    };
 
-[export]
-  path = /srv/raid/export
-  public = yes
-  only guest = yes
-  writable = yes
-'';
+    samba = {
+      enable = true;
+      nsswins = true;
+
+      settings = {
+        global = {
+          "workgroup" = "WORKGROUP";
+          "server string" = "tiamat";
+          "netbios name" = "tiamat";
+          "security" = "user";
+          # "use sendfile" = "yes";
+          # "max protocol" = "smb2";
+          # localhost includes the IPv6 loopback address, ::1.
+          "hosts allow" = "0.0.0.0/0 192.168.0. 127.0.0.1 localhost";
+          # "hosts deny" = "0.0.0.0/0";
+          "guest account" = "ctr";
+          "map to guest" = "bad user";
+        };
+
+        export = {
+          "path" = "/srv/raid/export";
+          "browseable" = "yes";
+          "writeable" = "yes";
+          "guest ok" = "yes";
+          "create mask" = "0644";
+          "directory mask" = "0755";
+          "force user" = "ctr";
+          "force group" = "nogroup";
+        };
+      };
+    };
+
+    samba-wsdd = {
+      enable = true;
+      openFirewall = true;
     };
 
     transmission = {
+      package = pkgs.transmission_4;
       enable = true;
 
       settings = {
